9beb484a84820fa99666b61db7dc51186ad4c0c0
ShinjungOh <tlswjd9260@gmail.com>
2022-04-28 22:13:58 +0900

    [ADD] habit tracker add form

--- 2022-04-22/habit-tracker/src/app.jsx
+++ 2022-04-22/habit-tracker/src/app.jsx
@@ -36,6 +36,11 @@ class App extends Component {
         this.setState({habits: habits});
     };
 
+    handleAdd = name => {
+        const habits = [...this.state.habits, {id: Date.now(), name: name, count: 0}];
+        this.setState({habits});
+    };
+
     render() {
         return (
             <>
@@ -45,6 +50,7 @@ class App extends Component {
                     onIncrement={this.handleIncrement}
                     onDecrement={this.handleDecrement}
                     onDelete={this.handleDelete}
+                    onAdd={this.handleAdd}
                 />
             </>
         );
