7250c65cf3f1a16e3b655078e306b7591004650c
ShinjungOh <tlswjd9260@gmail.com>
2022-05-03 23:29:22 +0900

    [ADD] PostCSS 모듈화

---
+++ 2022-05-03/PostCSS/test/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button1 from "./components/button1";
+import Button2 from "./components/button2";
+
+function App() {
+  return (
+      <>
+        <Button1/>
+        <Button2/>
+      </>
+  );
+}
+
+export default App;
