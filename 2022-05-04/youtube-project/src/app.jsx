4701b7678132e70f6c041998d4c7b0d416d3b75c
ShinjungOh <tlswjd9260@gmail.com>
2022-05-09 17:37:20 +0900

    [UPDATE] youtube search 리팩토링

--- 2022-05-04/youtube-project/src/app.jsx
+++ 2022-05-04/youtube-project/src/app.jsx
@@ -4,36 +4,16 @@ import VideoList from "./components/video-list/video-list";
 import SearchHeader from "./components/search-header/search-header";
 import styles from "./components/vedio-item/video-item.module.css";
 
-function App() {
+function App({youtube}) {
     const [videos, setVideos] = useState([]);
     const search = query => {
-        const requestOptions = {
-            method: 'GET',
-            redirect: 'follow',
-        };
-
-        fetch(
-            `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyBKYyzbeAxZxTMNsmAyNBsiMQLki64ue10`,
-            requestOptions)
-            .then(response => response.json())
-            .then(result => result.items.map(item => ({...item, id: item.id.videoId})))
-            .then(items => setVideos(items))
-            .catch(error => console.log('error', error));
+        youtube.search(query)
+            .then(videos => setVideos(videos));
     };
 
     useEffect(() => {
-        const requestOptions = {
-            method: 'GET',
-            redirect: 'follow',
-        };
-
-        fetch(
-            'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBKYyzbeAxZxTMNsmAyNBsiMQLki64ue10',
-            requestOptions
-        )
-            .then(response => response.json())
-            .then(result => setVideos(result.items))
-            .catch(error => console.log('error', error));
+        youtube.mostPopular()
+            .then(videos => setVideos(videos));
     }, []);
 
     return (
