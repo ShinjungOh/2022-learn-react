f42fb151963a404249066d3d6b751d70c85b2ff6
ShinjungOh <tlswjd9260@gmail.com>
2022-05-09 23:17:51 +0900

    [ADD] youtube video detail page

--- 2022-05-04/youtube-project/src/app.jsx
+++ 2022-05-04/youtube-project/src/app.jsx
@@ -1,25 +1,44 @@
 import React, {useEffect, useState} from "react";
-import './app.module.css';
+import styles from './app.module.css';
 import VideoList from "./components/video-list/video-list";
 import SearchHeader from "./components/search-header/search-header";
-import styles from "./components/vedio-item/video-item.module.css";
+import VideoDetail from "./components/video-detail/video-detail";
 
 function App({youtube}) {
     const [videos, setVideos] = useState([]);
+    const [selectedVideo, setSelectedVideo] = useState(null);
+
+    const selectVideo = (video) => {
+        setSelectedVideo(video);
+    };
+
     const search = query => {
-        youtube.search(query)
+        youtube
+            .search(query)
             .then(videos => setVideos(videos));
     };
 
     useEffect(() => {
-        youtube.mostPopular()
+        youtube
+            .mostPopular()
             .then(videos => setVideos(videos));
     }, []);
 
     return (
         <div className={styles.app}>
             <SearchHeader onSearch={search}/>
-            <VideoList videos={videos}/>;
+            <section className={styles.content}>
+                {selectedVideo && (
+                    <div className={styles.detail}>
+                        <VideoDetail video={selectedVideo}/>
+                    </div>
+                )}
+                <div className={styles.list}>
+                    <VideoList videos={videos}
+                               onVideoClick={setSelectedVideo}
+                               display={selectVideo ? 'list' : 'grid'}/>
+                </div>
+            </section>
         </div>
     );
 }
