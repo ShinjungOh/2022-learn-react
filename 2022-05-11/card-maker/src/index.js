e559bfaccdd357fe07717f55c764f0f14ea41c64
ShinjungOh <tlswjd9260@gmail.com>
2022-05-14 07:21:57 +0900

    [ADD] card-maker login UI, 기능 구현

--- 2022-05-11/card-maker/src/index.js
+++ 2022-05-11/card-maker/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.module.css';
 import App from './app';
+import AuthService from "./service/auth-service";
 
+const authService = new AuthService(process.env);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App authService={authService}/>
+    </React.StrictMode>
 );
--- 2022-05-11/card-maker/src/service/firebase.js
+++ 2022-05-11/card-maker/src/service/firebase.js
@@ -1,10 +1,16 @@
-import { initializeApp } from "firebase/app";
+//import {initializeApp} from 'firebase/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import firebase from 'firebase/compat/app';
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp;
