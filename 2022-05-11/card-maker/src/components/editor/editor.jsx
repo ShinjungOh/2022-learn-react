0597e9117609efc2ba180ae4e154318e51e72209
ShinjungOh <tlswjd9260@gmail.com>
2022-05-15 03:17:35 +0900

    [ADD] card-maker maker layout

--- 2022-05-11/card-maker/src/components/editor/editor.jsx
+++ 2022-05-11/card-maker/src/components/editor/editor.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import styles from './editor.module.css';
+import Card from "../card/card";
+import CardEditForm from "../card-edit-form/card-edit-form";
 
-const Editor = (props) => (
+const Editor = ({cards}) => (
     <section className={styles.editor}>
-        <h1 className={styles.title}>Card Preview</h1>
+        <h1 className={styles.title}>Card Maker</h1>
+        {cards.map(card => (
+            <CardEditForm card={card}/>
+        ))}
     </section>
 );
 
--- 2022-05-11/card-maker/src/components/maker/maker.jsx
+++ 2022-05-11/card-maker/src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './maker.module.css';
 import {useNavigate} from "react-router-dom";
 import Header from "../header/header";
@@ -7,6 +7,38 @@ import Editor from "../editor/editor";
 import Preview from "../preview/preview";
 
 const Maker = ({authService}) => {
+    const [cards, setCards] = useState([
+        {
+            id: '1',
+            name: 'SJ',
+            company: 'ABC',
+            theme: 'light',
+            email: 'tlswjd9260#gmail.com',
+            message: 'Hello',
+            fileName: 'SJ',
+            fileURL: 'SJ.png',
+        },
+        {
+            id: '2',
+            name: 'SJ',
+            company: 'ABC',
+            theme: 'light',
+            email: 'tlswjd9260#gmail.com',
+            message: 'Hello',
+            fileName: 'SJ',
+            fileURL: 'SJ.png',
+        },
+        {
+            id: '3',
+            name: 'SJ',
+            company: 'ABC',
+            theme: 'light',
+            email: 'tlswjd9260#gmail.com',
+            message: 'Hello',
+            fileName: 'SJ',
+            fileURL: 'SJ.png',
+        }
+    ]);
     const navigate = useNavigate();
     const onLogout = () => {
         authService.logout();
@@ -24,8 +56,8 @@ const Maker = ({authService}) => {
         <section className={styles.maker}>
             <Header onLogout={onLogout}/>
             <div className={styles.container}>
-                <Preview/>
-                <Editor/>
+                <Editor cards={cards} />
+                <Preview cards={cards} />
             </div>
             <Footer/>
         </section>
