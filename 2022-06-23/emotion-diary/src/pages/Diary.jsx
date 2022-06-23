0bb1a159fd4a4ede11cc2389c4c0b36dc3edd24f
ShinjungOh <tlswjd9260@gmail.com>
2022-06-23 20:13:41 +0900

    [ADD] emotion-diary project setup

---
+++ 2022-06-23/emotion-diary/src/pages/Diary.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Diary = () => {
+    return (
+        <div>
+            <h1>Diary</h1>
+            <p>diary</p>
+        </div>
+    );
+}
+
+export default Diary;
