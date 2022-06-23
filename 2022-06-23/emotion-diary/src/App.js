3c9a8bd2eb51998b031ea2717fe057266ef80899
ShinjungOh <tlswjd9260@gmail.com>
2022-06-24 04:54:41 +0900

    [ADD] emotion-diary 레이아웃, 이미지, header component 세팅

--- 2022-06-23/emotion-diary/src/App.js
+++ 2022-06-23/emotion-diary/src/App.js
@@ -4,20 +4,36 @@ import Home from "./pages/Home";
 import New from './pages/New';
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
-import RouteTest from "./components/RouteTest";
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
+                <MyHeader headText={"APP"}
+                          leftChild={<MyButton text={"왼쪽 버튼"} onClick={() => alert("left")}/>}
+                          rightChild={<MyButton text={"오른쪽 버튼"} onClick={() => alert("right")}/>}/>
                 <h2>Hello</h2>
+
+                <MyButton text={"버튼"}
+                          onClick={() => alert("버튼 클릭")}
+                          type={"positive"}/>
+
+                <MyButton text={"버튼"}
+                          onClick={() => alert("버튼 클릭")}
+                          type={"negative"}/>
+
+                <MyButton text={"버튼"}
+                          onClick={() => alert("버튼 클릭")}
+                />
+
                 <Routes>
                     <Route path="/" element={<Home/>}/>
                     <Route path="/new" element={<New/>}/>
                     <Route path="/edit" element={<Edit/>}/>
                     <Route path="/diary/:id" element={<Diary/>}/>
                 </Routes>
-                <RouteTest/>
             </div>
         </BrowserRouter>
     );
