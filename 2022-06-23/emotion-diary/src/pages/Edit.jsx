2d7b3f6d3ff136e6633dc7a369094a5e54229ce8
ShinjungOh <tlswjd9260@gmail.com>
2022-06-23 22:23:20 +0900

    [ADD] emotion-diary react router 화면 전환

--- 2022-06-23/emotion-diary/src/pages/Edit.jsx
+++ 2022-06-23/emotion-diary/src/pages/Edit.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import {useNavigate, useSearchParams} from "react-router-dom";
 
 const Edit = () => {
+    const navigate = useNavigate();
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const id = searchParams.get('id');
+    console.log("id : ", id);
+
+    const mode = searchParams.get('mode');
+    console.log(mode);
+
     return (
         <div>
             <h1>Edit</h1>
             <p>edit</p>
+            <button onClick={() => setSearchParams({who: 'SJ'})}>QS 바꾸기</button>
+            <button onClick={() => {
+                navigate("/home");
+            }}>
+                🏠 Home으로 가기
+            </button>
+            <button onClick={() => {
+                navigate(-1);
+            }}>
+                뒤로가기
+            </button>
         </div>
     );
 }
