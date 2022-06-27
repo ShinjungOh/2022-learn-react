0bc888fde8a0c1af52ddb18f7cce26b7d38cd4e1
ShinjungOh <tlswjd9260@gmail.com>
2022-06-27 22:49:18 +0900

    [ADD] emotion-diary '/new' page 구현: DiaryEditor component 추가, 날짜 입력란 css

---
+++ 2022-06-23/emotion-diary/src/components/DiaryEditor.jsx
@@ -0,0 +1,46 @@
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import MyHeader from "./MyHeader";
+import MyButton from "./MyButton";
+
+const getStringDate = (date) => {
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    if (month < 10) {
+        month = `0${month}`;
+    }
+    if (day < 10) {
+        day = `0${day}`;
+    }
+    return `${year}-${month}-${day}`;
+    // return date.toISOString().slice(0, 10);
+}
+
+const DiaryEditor = () => {
+    const [date, setDate] = useState(getStringDate(new Date()));
+    const navigate = useNavigate();
+
+    return (
+        <div className="DiaryEditor">
+            <MyHeader
+                headText={"새 일기쓰기"}
+                leftChild={<MyButton text={"뒤로가기"} onClick={() => navigate(-1)}/>}
+            />
+            <div>
+                <section>
+                    <h4>오늘은 며칠인가요?</h4>
+                    <div className="input_box">
+                        <input className="input_date"
+                               value={date}
+                               type="date"
+                               onChange={e => setDate(e.target.value)}
+                        />
+                    </div>
+                </section>
+            </div>
+        </div>
+    );
+}
+
+export default DiaryEditor;
--- 2022-06-23/emotion-diary/src/pages/New.jsx
+++ 2022-06-23/emotion-diary/src/pages/New.jsx
@@ -1,48 +1,12 @@
-import React, {useState} from "react";
-import MyHeader from "../components/MyHeader";
-import MyButton from "../components/MyButton";
-import {useNavigate} from "react-router-dom";
-
-
-const getStringDate = (date) => {
-    let year = date.getFullYear();
-    let month = date.getMonth() + 1;
-    let day = date.getDate();
-    if (month < 10) {
-        month = `0${month}`;
-    }
-    if (day < 10) {
-        day = `0${day}`;
-    }
-    return `${year}-${month}-${day}`;
-    // return date.toISOString().slice(0, 10);
-}
-
+import React from "react";
+import DiaryEditor from "../components/DiaryEditor";
 
 const New = () => {
-    const [date, setDate] = useState(getStringDate(new Date()));
-    const navigate = useNavigate();
-
-    return (
-        <div>
-            <MyHeader
-                headText={"새 일기쓰기"}
-                leftChild={<MyButton text={"뒤로가기"} onClick={() => navigate(-1)}/>}
-            />
-            <div>
-                <section>
-                    <h4>오늘은 며칠인가요?</h4>
-                    <div className="input_box">
-                        <input className="input_date"
-                               value={date}
-                               type="date"
-                               onChange={e => setDate(e.target.value)}
-                        />
-                    </div>
-                </section>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+        <DiaryEditor/>
+    </div>
+  );
 }
 
 export default New;
