75312c694619e06b2ac0b970358e327382219e69
ShinjungOh <tlswjd9260@gmail.com>
2022-07-27 20:03:09 +0900

    [ADD] infinite scroll: intersection observer

--- 2022-07-27/Q1/index.js
+++ 2022-07-27/Q1/index.js
@@ -1,6 +1,5 @@
 import "style.css";
 import renderList from "./listRenderer";
-import {debounce} from './util';
 
 const app = document.querySelector("#app");
 const fetchMoreTrigger = document.querySelector("#fetchMore");
@@ -13,27 +12,20 @@ const fetchMore = async () => {
     target.classList.remove("loading");
 };
 
-const onScroll = e => {
-    // console.dir(e.target.scrollingElement);
-    const {
-        scrollHeight,
-        scrollTop,
-        clientHeight,
-    } = e.target.scrollingElement;
-    console.log(scrollTop);  // 성능 저하의 요인
-    if (scrollTop + clientHeight === scrollHeight) {
-        fetchMore();
-    }
-};
+const fetchMoreObserver = new IntersectionObserver(
+    // entry => {
+    //     console.log(entry);
+    // });
 
-document.addEventListener("scroll", debounce(onScroll, 300));
+    ([{isIntersecting}]) => {
+        if (isIntersecting) fetchMore();
+    }
+);
+fetchMoreObserver.observe(fetchMoreTrigger);
+
 fetchMore();
 
 
 /**
- * scroll 이벤트는 매우 많이 발생하므로 성능 저하가 우려된다. ‘fetchMore’ 동작은 스크롤의 최하단에서만 발생하므로 스크롤 이벤트 중 마지막 값에 대해서만 반응하는 것으로 충분하다.
- * 이를 위한 기법 throttle / debounce
- *
- * throttle : 일정 시간 간격으로 한번씩만 실행
- * debounce : 연속으로 발생하는 이벤트에 대해 마지막 한 번만 실행
+ *  IntersectionObserver는 자바스크립트 메인 쓰레드에서 실행되는 Event Listener가 아닌 브라우저에서 별도로 마련한 API이므로 성능 저하의 우려가 적다.
  */
